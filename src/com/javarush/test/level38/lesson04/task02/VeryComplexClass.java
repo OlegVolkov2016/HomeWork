8bf5fce9c2c919a1b344cf88e50db2ffda1a6471
OlegVolkov2016 <novasoft.print@gmail.com>
2016-02-20 12:15:13 +0200

    JavaRushHomeWork 20/02/2016 12:10

---
+++ src/com/javarush/test/level38/lesson04/task02/VeryComplexClass.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level38.lesson04.task02;
+
+/* Непроверяемые исключения (unchecked exception)
+Напиши реализацию метода methodThrowsClassCastException(). Он должен
+всегда кидать непроверяемое исключение ClassCastException.
+
+Напиши реализацию метода methodThrowsNullPointerException(). Он должен
+всегда кидать непроверяемое исключение NullPointerException.
+
+Кинуть исключение (throw) явно нельзя.
+*/
+
+public class VeryComplexClass {
+    public void methodThrowsClassCastException() {
+        Object s = "A";
+        Integer i = (Integer) s;
+    }
+
+    public void methodThrowsNullPointerException() {
+        //напишите тут ваш код
+        String s = null;
+        s.length();
+    }
+}
---
+++ src/com/javarush/test/level39/lesson09/big01/LogParser.java
@@ -0,0 +1,4 @@
+package com.javarush.test.level39.lesson09.big01;
+
+public class LogParser {
+}
